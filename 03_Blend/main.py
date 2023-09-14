786dc8626910a1318a8a4dd4cb1f0159270c424b
Head <zelzakegui@gmail.com>
2023-09-13 23:39:56 -0300

    Added blend and addImage methods

--- 03_Blend/main.py
+++ 03_Blend/main.py
@@ -13,52 +13,74 @@ def resizeImage(image, scale):
     width = int(w * scale/100)
     return cv2.resize(image, dsize=(width, height))
 
-def addImageOverlay(background, foreground, translForeH, translForeW):
-    backH, backW, _ = background.shape
+def resizeEqual(imageSizeFrom, imageSizeTo):
+    h, w, c = imageSizeFrom.shape
+    height = int(h)
+    width = int(w)
+    return cv2.resize(imageSizeTo, (width, height))
+
+def blend(imgA, imgB):
+    resImage = cv2.addWeighted(imgA, 0.8, imgB, 0.2, 0)
+    return resImage
+
+'''def addOverlay(background, foreground):
     foreH, foreW, _ = foreground.shape
-    restH, restW = backH - foreH, backW - foreW
 
-    background = resizeImage(background, 300)
-
-    print(background.shape)
-    print(foreground.shape)
-    
-    crop = background[translForeH : foreH + translForeH, translForeW: foreW + translForeW]
+    backgroundWithNoImage = background[0 : foreH, 0 : foreW]
 
     foregroundGray = cv2.cvtColor(foreground, cv2.COLOR_BGR2GRAY)
-
-    display(foregroundGray)
-
-    '''display(background)
-    display(foreground)
-    display(crop)'''
+    ret, maskFore = cv2.threshold(foregroundGray, 245, 255, cv2.THRESH_BINARY)
 
 
-def blending(img1, img2, fundo):
-    imgA = cv2.imread(img1)
-    imgB = cv2.imread(fundo)
-    imgB = resizeImage(imgB, 400)
-    imgFinal = addImageOverlay(fundo, imgB, 200, 200)
+    backWithMask = cv2.bitwise_and(backgroundWithNoImage, backgroundWithNoImage, mask = maskFore)
+    foreWithMask = cv2.bitwise_not(maskFore)
+    foreWithMask = cv2.bitwise_and(foreground, foreground, mask = foreWithMask)
+
+    result = cv2.add(backWithMask, foreWithMask)
+    copyImage = background.copy()
+
+    copyImage[0 : foreH + 30, 0 : foreW + 100]
+
+    return copyImage'''
+
+def addImageNoBackground(foreground, background):
+    foreH, foreW, _ = foreground.shape
+    #backH, backW, _ = background.shape
+
+    foregroundGray = cv2.cvtColor(foreground, cv2.COLOR_BGR2GRAY)
+    ret, maskFore = cv2.threshold(foregroundGray, 75, 255, cv2.THRESH_BINARY)
+
+    #display(foregroundGray)
+
+    backgroundWithNoImage = background[0 : foreH, 0 : foreW]
+
+    backWithMask = cv2.bitwise_and(backgroundWithNoImage, backgroundWithNoImage, mask = maskFore)
+    foreWithMask = cv2.bitwise_not(maskFore)
+    foreWithMask = cv2.bitwise_and(foreground, foreground, mask = foreWithMask)
+
+    result = cv2.add(foreWithMask, backWithMask)
+
+    return result
+
+
+
+def addImage(imgA, imgB):
+    return cv2.add(imgA, imgB)
 
 def main():
-    imgA = cv2.imread("img/A.jpeg")
-    imgB = cv2.imread("img/B.jpeg")
+    imgA = cv2.imread('img/0.jpeg')
+    imgB = cv2.imread('img/0_2.jpeg')
+    imgC = cv2.imread('img/D.png')
+    imgB = resizeEqual(imgA, imgB)
+    imgC = resizeEqual(imgA, imgC)
 
-    addImageOverlay(imgA, imgB, 100, 200)
+    newImg = cv2.cvtColor(blend(imgA, imgB), cv2.COLOR_BGR2RGB)
 
+    display(newImg)
 
-    '''
-    alturaA, larguraA, _ = imgA.shape
-    imgB = cv2.resize(imgB, (larguraA, alturaA))
+    display(addImageNoBackground(imgC, newImg))
 
-    imgAB = cv2.addWeighted(imgA, 0.5, imgB, 0.5, 0)
-
-    alturaAB, larguraAB, _ = imgAB.shape
-    imgAB = cv2.resize(imgAB, (int(larguraAB * 0.5), int(alturaAB * 0.5)))
-
-    cv2.imshow(imgAB)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    '''
+    '''newImg = cv2.cvtColor(addOverlay(imgB, imgA), cv2.COLOR_BGR2RGB)
+    display(newImg)'''
 
 main()
